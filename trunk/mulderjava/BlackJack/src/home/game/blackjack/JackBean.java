d46cfab891c9f53adc779146afa8da9d5969cdde
darkwindstom <darkwindstom@ec750ce0-f50d-11de-aa3c-59de0406b4f5>
2009-12-30 07:02:35 +0000

    version 1.0

---
+++ trunk/mulderjava/BlackJack/src/home/game/blackjack/JackBean.java
@@ -0,0 +1,42 @@
+package home.game.blackjack;
+
+public class JackBean {
+	
+	private String sn = "";
+	private String name = "";
+	private String num = "";
+	private Integer value = 0;
+	private String use = "";
+	
+	public String getSn() {
+		return sn;
+	}
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public String getUse() {
+		return use;
+	}
+	public void setUse(String use) {
+		this.use = use;
+	}
+
+}
